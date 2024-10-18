8006641572cde4e2d79232e343cfce3ee7cf7f84
Sandalcho7 <bahurel.anatole@gmail.com>
2024-10-18 14:57:37 +0200

    added loki to docker-compose, winston logger to api

--- src/server.js
+++ src/server.js
@@ -2,6 +2,8 @@ import express from 'express';
 import dotenv from 'dotenv';
 import promMiddleware from 'express-prometheus-middleware';
 import promClient from 'prom-client';
+import winston from 'winston';
+import LokiTransport from 'winston-loki';
 
 // Load environment variables
 dotenv.config();
@@ -13,6 +15,26 @@ const port = process.env.API_PORT || 3000;
 // Create a Registry to register metrics
 const register = new promClient.Registry();
 
+// Configure Winston logger with Loki transport
+const logger = winston.createLogger({
+  level: 'info',
+  format: winston.format.json(),
+  defaultMeta: { service: 'api-server' },
+  transports: [
+    new winston.transports.Console({
+      format: winston.format.simple(),
+    }),
+    new LokiTransport({
+      host: 'http://loki:3100',
+      labels: { job: 'api-server' },
+      json: true,
+      format: winston.format.json(),
+      replaceTimestamp: true,
+      onConnectionError: (err) => console.error(err),
+    }),
+  ],
+});
+
 // Add Prometheus middleware
 app.use(promMiddleware({
   metricsPath: '/metrics',
@@ -22,14 +44,24 @@ app.use(promMiddleware({
   responseLengthBuckets: [512, 1024, 5120, 10240, 51200, 102400]
 }));
 
+// Middleware to log all requests
+app.use((req, res, next) => {
+  logger.info(`${req.method} ${req.url}`, {
+    method: req.method,
+    url: req.url,
+    ip: req.ip,
+  });
+  next();
+});
+
 // Routes
 app.get('/test', (req, res) => {
-  console.log('Test endpoint was called!');
+  logger.info('Test endpoint was called!');
   res.send(`Test endpoint successful. Running on ${host}:${port}`);
 });
 
 // Start the server
 app.listen(port, host, () => {
-  console.log(`Server running on http://${host}:${port}`);
-  console.log(`Metrics available at http://${host}:${port}/metrics`);
+  logger.info(`Server running on http://${host}:${port}`);
+  logger.info(`Metrics available at http://${host}:${port}/metrics`);
 });
